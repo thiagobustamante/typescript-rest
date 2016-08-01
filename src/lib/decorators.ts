543d41b343111839c288d0c5fa65b277576ede8c
thiagobustamante <thiago@cruxframework.org>
2016-08-01 16:32:27 -0300

    Improve documentation

--- src/lib/decorators.ts
+++ src/lib/decorators.ts
@@ -302,77 +302,385 @@ export function ContextAccept(...args: any[]) {
     throw new Error("Invalid @ContextAccept Decorator declaration.");	
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP GET requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   getPeople() {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * GET http://mydomain/people 
+ * ```
+ */
 export function GET(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor){
     processHttpVerb(target, propertyKey, HttpMethod.GET);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP POST requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ POST
+ *   addPerson() {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * POST http://mydomain/people 
+ * ```
+ */
 export function POST(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.POST);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP PUT requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ PUT
+ *   @ Path(":id")
+ *   savePerson(person: Person) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * PUT http://mydomain/people/123 
+ * ```
+ */
 export function PUT(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.PUT);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP DELETE requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ DELETE
+ *   @ Path(":id")
+ *   removePerson(@ PathParam("id")id: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * PUT http://mydomain/people/123 
+ * ```
+ */
 export function DELETE(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.DELETE);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP HEAD requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ HEAD
+ *   headPerson() {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * HEAD http://mydomain/people/123 
+ * ```
+ */
 export function HEAD(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.HEAD);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP OPTIONS requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ OPTIONS
+ *   optionsPerson() {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * OPTIONS http://mydomain/people/123 
+ * ```
+ */
 export function OPTIONS(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.OPTIONS);
 }
 
+/**
+ * A decorator to tell the [[Server]] that a method 
+ * should be called to process HTTP PATCH requests.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ PATCH
+ *   @ Path(":id")
+ *   savePerson(person: Person) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * PATCH http://mydomain/people/123 
+ * ```
+ */
 export function PATCH(target: any, propertyKey: string,
 	descriptor: PropertyDescriptor) {
     processHttpVerb(target, propertyKey, HttpMethod.PATCH);
 }
 
+/**
+ * Creates a mapping between a fragment of the requested path and 
+ * a method argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   @ Path(":id")
+ *   getPerson(@ PathParam("id") id: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * GET http://mydomain/people/123 
+ * ```
+ * 
+ * And pass 123 as the id argument on getPerson method's call.
+ */
 export function PathParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.path, name);
 	}
 }
 
+/**
+ * Creates a mapping between a file on a multipart request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ POST
+ *   @ Path("id")
+ *   addAvatar(@ PathParam("id") id: string,
+ *             @ FileParam("avatar") file: Express.Multer.File) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests and bind the 
+ * file with name "avatar" on the requested form to the file 
+ * argument on addAvatar method's call.
+ */
 export function FileParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.file, name);
 	}
 }
 
+/**
+ * Creates a mapping between a list of files on a multipart request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ POST
+ *   @ Path("id")
+ *   addAvatar(@ PathParam("id") id: string,
+ *             @ FilesParam("avatar") Array<file>: Express.Multer.File) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests and bind the 
+ * files with name "avatar" on the request form to the file
+ * argument on addAvatar method's call.
+ */
 export function FilesParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.files, name);
 	}
 }
 
+/**
+ * Creates a mapping between a query parameter on request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   getPeople(@ QueryParam("name") name: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests like:
+ *
+ * ```
+ * GET http://mydomain/people?name=joe 
+ * ```
+ * 
+ * And pass "joe" as the name argument on getPerson method's call.
+ */
 export function QueryParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.query, name);
 	}
 }
 
+/**
+ * Creates a mapping between a header on request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   getPeople(@ HeaderParam("header") header: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests and bind the 
+ * header called "header" to the header argument on getPerson method's call.
+ */
 export function HeaderParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.header, name);
 	}
 }
 
+/**
+ * Creates a mapping between a cookie on request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   getPeople(@ CookieParam("cookie") cookie: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests and bind the 
+ * cookie called "cookie" to the cookie argument on getPerson method's call.
+ */
 export function CookieParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.cookie, name);
 	}
 }
 
+/**
+ * Creates a mapping between a form parameter on request and a method
+ * argument.
+ * 
+ * For example:
+ *
+ * ```
+ * @ Path("people")
+ * class PeopleService {
+ *   @ GET
+ *   getPeople(@ FormParam("name") name: string) {
+ *      // ...
+ *   }
+ * }
+ * ```
+ *
+ * Will create a service that listen for requests and bind the 
+ * request paramenter called "name" to the name argument on getPerson 
+ * method's call.
+ */
 export function FormParam(name: string) {
     return function(target: Object, propertyKey: string, parameterIndex: number) {
 		processDecoratedParameter(target, propertyKey, parameterIndex, metadata.ParamType.form, name);
