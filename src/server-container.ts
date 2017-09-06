90cbe3202c506d8950bbf8bff46cce58e7b155e7
Alexey Balchunas <bleshik@gmail.com>
2017-09-06 06:29:54 +0300

    Fixed a potential 'run-away promise' bluebird warning

--- src/server-container.ts
+++ src/server-container.ts
@@ -344,6 +344,7 @@ export class InternalServer {
                     Promise.resolve(value)
                     .then((val: any) => {
                         this.sendValue(val, res, next);
+                        return null;
                     }).catch((err: any) => {
                         next(err);
                     });
